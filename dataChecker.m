aa40f31c021d733df66a01270c14f211fb49b91f
ppxma7 <michael.asghar@nottingham.ac.uk>
2026-09-15 15:06:16 +0100

    updates

--- dataChecker.m
+++ dataChecker.m
@@ -1,7 +1,6 @@
 % script to quickly check outputs of the EMG_reader
 %
 % MA June 2026
-
 close all
 clear all
 clc
@@ -13,7 +12,6 @@ load(fullpath);
 
 %% Force + Target
 t = (0:length(Force)-1)/signal.fsamp;
-
 figure
 plot(t,Force(1,:),'-r'); hold on
 plot(t,Force(2,:),'-b')
@@ -25,6 +23,11 @@ plot(t,Force(7,:),'-m')
 legend(signal.auxiliaryname, 'Interpreter', 'none')
 title('Force channels'); xlabel('Time (s)'); ylabel('Volts')
 
+%% Auxiliary channels
+figure
+plot(signal.auxiliary')
+legend(signal.auxiliaryname)
+title('Auxiliary channels')
 
 %% EMG channels (overlaid per grid)
 nChan = signal.nChan;
@@ -40,13 +43,56 @@ for g = 1:ngrid
     idx = (g-1)*chPerGrid + (1:chPerGrid);
     nexttile
     plot(t, signal.data(idx,:))
-    title(sprintf('%s (%s) - %d channels', signal.muscle{g}, signal.gridname{g}, chPerGrid))
+    title(sprintf('Grid %d (%s) - %d channels', g, signal.gridname{g}, chPerGrid))
     xlabel('Time (s)'); ylabel('Amplitude')
     axis tight
 end
 
-%% Auxiliary channels
-figure
-plot(signal.auxiliary')
-legend(signal.auxiliaryname)
-title('Auxiliary channels')
+%% EMG channels (Stacked / Waterfall per Grid)
+nChan = signal.nChan;
+ngrid = signal.ngrid;
+chPerGrid = nChan/ngrid;
+t = (0:size(signal.data,2)-1)/signal.fsamp;
+
+fprintf('Total channels: %d, Grids: %d, Channels per grid: %d\n', nChan, ngrid, chPerGrid);
+
+for g = 1:ngrid
+    idx = (g-1)*chPerGrid + (1:chPerGrid);
+    gridData = signal.data(idx, :);
+
+    % Compute vertical spacing step based on overall peak-to-peak amplitude
+    % Compute vertical spacing step using range (max - min) across time (dimension 2)
+    offsetStep = max(range(gridData, 2));
+    if offsetStep == 0; offsetStep = 1; end % Prevents zero spacing if signals are flat
+
+    % Create a matrix of offsets for vectorised plotting
+    offsets = (0:chPerGrid-1)' * offsetStep;
+    stackedData = gridData + offsets;
+    
+    
+    figure('Name', sprintf('Grid %d - %s', g, signal.muscle{g}));
+    plot(t, stackedData, 'Color',[0.4 0.8 0.4]);
+    % 
+    set(gcf,'color','black')
+
+    ax = gca;
+    ax.Color = 'k';                     % Black background inside plot
+    ax.XColor = 'w';                     % White X-axis line & labels
+    ax.YColor = 'w';                     % White Y-axis line & labels
+    
+    % Force dark grid lines
+    grid on
+    ax.GridColorMode = 'manual';         % Disable auto grid color calculation
+    ax.GridColor = [0.25 0.25 0.25];     % Dark grey grid lines (adjust closer to 0 for darker)
+    ax.GridAlpha = 1;
+    
+    % Formatting
+    title(sprintf('EMG Stacked: %s (%s) - %d channels', signal.muscle{g}, signal.gridname{g}, chPerGrid))
+    xlabel('Time (s)')
+    ylabel('Channels (Offset)')
+    yticks(offsets)
+    yticklabels(cellstr(num2str((1:chPerGrid)')))
+    axis tight
+    grid on
+end
+
